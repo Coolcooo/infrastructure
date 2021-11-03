92e11499ab0e5f16adfdb72e0df77ca51b7b5a09
Vladimir Privezenov <privezenoff@yandex.ru>
2021-11-04 01:37:14 +0300

    refactor: add formatting

--- scripts/create-image.sh
+++ scripts/create-image.sh
@@ -3,7 +3,7 @@
 NAME_BUILD='release-app:'"$LATEST_RELEASE"
 docker build -t "$NAME_BUILD" .
 
-LOGS="${LOGS}"'DOCKER собрал образ с версией '"${NAME_BUILD}"
+LOGS=$(echo "${LOGS}"'\nDOCKER собрал образ с версией '"${NAME_BUILD}"'\n' | grep D)
 docker images
 chmod +x ./scripts/create-tracker-task.sh
 ./scripts/create-tracker-task.sh
