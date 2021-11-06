83d9ab7191b51511acc128820db97f9f97f39af1
rus <sr@trinitydigital.ru>
2021-11-06 14:03:17 +0300

    feat: test feat

--- test/unit/example.test.js
+++ test/unit/example.test.js
@@ -1,6 +1,6 @@
 describe('Simple Test Case', () => {
     it('Should return 4', () => {
-        expect(2 + 2).toBe(4);
+        expect(2 + 2).toBe(5);
     });
     it('Should return 5', () => {
         expect(3 + 2).toBe(5);
